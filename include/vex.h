92aaa1e8cc509712ad33fd60d5c99c85ea1c895a
Colby Prescott <crprescott2026@rsu14.org>
2024-04-12 22:01:14 -0400

    Added vex.h header guards

--- include/vex.h
+++ include/vex.h
@@ -1,3 +1,6 @@
+#ifndef VEX_H
+#define VEX_H
+
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,10 +10,12 @@
 #include "v5_vcs.h"
 
 
-#define waitUntil(condition)                                                   \
-  do {                                                                         \
-    wait(5, msec);                                                             \
-  } while (!(condition))
+#define waitUntil(condition)                                                     \
+    do {                                                                         \
+        wait(5, msec);                                                           \
+    } while (!(condition))
 
-#define repeat(iterations)                                                     \
-  for (int iterator = 0; iterator < iterations; iterator++)
+    #define repeat(iterations)                                                   \
+        for (int iterator = 0; iterator < iterations; iterator++)
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -10,19 +10,14 @@
 
 using namespace vex;
 
-// A global instance of vex::brain used for printing to the V5 brain screen
-vex::brain       Brain;
-
-// define your global instances of motors and other devices here
-
+vex::brain Brain;
 
 int main() {
+    Brain.Screen.print("Hewwo UwU");
 
-    Brain.Screen.printAt( 10, 50, "Hello V5" );
-   
-    while(1) {
-        
-        // Allow other tasks to run
-        this_thread::sleep_for(10);
-    }
+        while(1) {
+            
+            // Allow other tasks to run
+            this_thread::sleep_for(10);
+        }
 }
