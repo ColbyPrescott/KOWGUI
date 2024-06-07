a0912849d9072bae102c63b49d65ca355588c9e7
Colby Prescott <crprescott2026@rsu14.org>
2024-06-07 12:39:10 -0400

    PreTick demo changes text too. Fixed missing header in text.h

--- include/KOWGUI/Nodes/Visible/text.h
+++ include/KOWGUI/Nodes/Visible/text.h
@@ -7,6 +7,7 @@
 
 #include <string>
 #include <memory>
+#include <stdio.h>
 
 namespace KOWGUI {
 
--- src/TestGUI/Panels/preTickDemo.cpp
+++ src/TestGUI/Panels/preTickDemo.cpp
@@ -8,6 +8,7 @@ using namespace KOWGUI;
 void InitGUIPreTickDemo() {
     void (*hoverAnimation)(BaseNode*) = [](BaseNode* thisNode){
         thisNode->SetY((sin(vexSystemTimeGet() / 1000.0) + 1) / 2.0 * 200);
+        ((Text*)thisNode)->SetText("This node is animated! %d", (int)(vexSystemTimeGet() / 1000.0 * 2.0) % 10);
     };
 
     panels.preTickDemo->AddChildren({
