e1f21640f86d4cabadad70f840f773d8ea0dd280
Colby Prescott <crprescott2026@rsu14.org>
2024-06-24 12:51:46 -0400

    Text alignment demo panels show multiline text

--- src/TestGUI/Panels/textHorizontalAlignDemo.cpp
+++ src/TestGUI/Panels/textHorizontalAlignDemo.cpp
@@ -7,13 +7,13 @@ using namespace KOWGUI;
 void InitGUITextHorizontalAlignDemo() {
     panels.textHorizontalAlignDemo->AddChildren({
         (new Rectangle)->SetPosition(20, 20)->SetSize(480 - sidebarWidth - 40, 60)->SetFillColor(Color::gray)->AddChildren({
-            (new Text)->SetText("Left")->SetFontSize(20)->SetFontAlign(FontAlign::ascender)->SetHorizontalAlign(HorizontalAlign::left)
+            (new Text)->SetText("Left\nMultiline")->SetFontSize(20)->SetFontAlign(FontAlign::ascender)->SetHorizontalAlign(HorizontalAlign::left)
         }),
         (new Rectangle)->SetPosition(20, 90)->SetSize(480 - sidebarWidth - 40, 60)->SetFillColor(Color::gray)->AddChildren({
-            (new Text)->SetText("Center")->SetFontSize(20)->SetFontAlign(FontAlign::ascender)->SetHorizontalAlign(HorizontalAlign::center)
+            (new Text)->SetText("Center\nMultiline")->SetFontSize(20)->SetFontAlign(FontAlign::ascender)->SetHorizontalAlign(HorizontalAlign::center)
         }),
         (new Rectangle)->SetPosition(20, 160)->SetSize(480 - sidebarWidth - 40, 60)->SetFillColor(Color::gray)->AddChildren({
-            (new Text)->SetText("Right")->SetFontSize(20)->SetFontAlign(FontAlign::ascender)->SetHorizontalAlign(HorizontalAlign::right)
+            (new Text)->SetText("Right\nMultiline")->SetFontSize(20)->SetFontAlign(FontAlign::ascender)->SetHorizontalAlign(HorizontalAlign::right)
         }),
     });
 }
