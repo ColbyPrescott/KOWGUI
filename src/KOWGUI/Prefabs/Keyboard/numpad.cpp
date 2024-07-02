f07266b0c8046e1bab1b15fb3be112050dfb525c
Colby Prescott <crprescott2026@rsu14.org>
2024-07-02 19:30:48 -0400

    Numpad keys affect typing number

--- src/KOWGUI/Prefabs/Keyboard/numpad.cpp
+++ src/KOWGUI/Prefabs/Keyboard/numpad.cpp
@@ -3,10 +3,13 @@
 #include "KOWGUI/kowgui.h"
 
 #include <memory>
+#include <math.h>
 
 using namespace KOWGUI;
 
 namespace {
+
+    // Variables for parts of the GUI
 
     const int windowBarHeight = 20;
 
@@ -18,23 +21,83 @@ namespace {
     std::shared_ptr<Color> buttonFocusedColor((new Color)->SetHex("#a3a3a3"));
     std::shared_ptr<Color> highlightColor((new Color)->SetHex("#ffffff"));
 
-    void TypeDigitFromDataAtEnd(BaseNode* thisNode) {
 
+
+    // Variables for numpad functionality
+
+    // Representation of the number currently being typed. Represented as one giant integer for simpler code
+    int32_t typingNumberAsInteger = 0;
+    // Whether or not decimals are currently being typed
+    bool typingDecimals = false;
+    // How many digits of typingNumberAsInteger are placed after the decimal
+    int numTypingDecimalDigits = 0;
+
+
+    // Functions for numpad functionality
+
+    // Called from each digit key
+    void TypeDigitFromDataAtEnd(BaseNode* thisNode) {
+        int digitToAdd = *(int*)((Data*)thisNode->FindShallowID("data"))->GetProperty("keyDigit");
+
+        // Shift all digits left by one digit, making room for a zero, then converted to new digit by adding it
+        typingNumberAsInteger *= 10;
+        typingNumberAsInteger += digitToAdd;
+
+        // If currently typing decimals, this new digit should add to the number of digits after the decimal
+        if(typingDecimals) numTypingDecimalDigits++;
     }
 
+    // Called from the backspace key Clickable node
+    void RemoveDigitFromEnd(BaseNode* thisNode) {
+        // Edge case where the decimal key was pressed, then no digits were typed
+        if(typingDecimals && numTypingDecimalDigits == 0) {
+            // Just stop the mode for typing decimals
+            typingDecimals = false;
+            // No need to do anything else
+            return;
+        }
+
+        // Shift all digits right by one digit. typingNumberAsInteger is an integer, so the shifted digit automatically gets cut off
+        typingNumberAsInteger /= 10;
+
+        // If currently typing decimals, this removed digit should remove from the number of digits after the decimal
+        if(typingDecimals) numTypingDecimalDigits--;
+        // If the last decimal was removed, stop typing decimal mode
+        if(numTypingDecimalDigits == 0) typingDecimals = false;
+    }
+
+    // Called from the decimal key
+    void StartTypingDecimal(BaseNode* thisNode) {
+        // Enable mode for typing decimals. If it's already true, this won't change anything
+        typingDecimals = true;
+    }
+
+    // Called from the typing number Text node
+    void UpdateTypingTextNode(BaseNode* thisNode) {
+        // If not typing a decimal, simply display the integer as is
+        if(!typingDecimals) ((Text*)thisNode)->SetText("%d", typingNumberAsInteger);
+        else {
+            // If typing decimal, display it by splitting the number before and after the decimal character and putting a period in between
+            int digitsBeforeDecimal = floor((double)typingNumberAsInteger / pow(10, numTypingDecimalDigits));
+            int digitsAfterDecimal = typingNumberAsInteger % (int)pow(10.0, (double)numTypingDecimalDigits);
+            ((Text*)thisNode)->SetText("%d.%d", digitsBeforeDecimal, digitsAfterDecimal);
+        }
+    }
+
+    // Called from the close button Clickable node
     void CloseNumpad(BaseNode* thisNode) {
         thisNode->parent->parent->SetDisabled(true);
     }
 
     // Template prefab for a standard key like 1, 2, or 3
-    Clickable* CreateKey(char keyCharacter) {
+    Clickable* CreateDigitKey(int keyDigit) {
         return (new Clickable)->SetRelease(TypeDigitFromDataAtEnd)->AddChildren({
             (new NFocused)->AddChildren({(new Rectangle)->SetFillColor(buttonNFocusedColor.get())->SetOutlineColor(highlightColor.get())}),
             (new Focused)->AddChildren({(new Rectangle)->SetFillColor(buttonFocusedColor.get())->SetOutlineColor(highlightColor.get())}),
 
-            (new Text)->SetPosition(1, 2)->SetText("%c", keyCharacter)->SetFont(Fonts::monospace)->SetFontSize(25)->SetAlignments(HorizontalAlign::center, VerticalAlign::middle)->SetColor(highlightColor.get()),
+            (new Text)->SetPosition(1, 2)->SetText("%d", keyDigit)->SetFont(Fonts::monospace)->SetFontSize(25)->SetAlignments(HorizontalAlign::center, VerticalAlign::middle)->SetColor(highlightColor.get()),
 
-            (new Data)->SetShallowID("data")->SetProperty("keyCharacter", new char(keyCharacter)),
+            (new Data)->SetShallowID("data")->SetProperty("keyDigit", new int(keyDigit)),
         });
     }
 
@@ -74,30 +137,30 @@ Group* Keyboard::CreateNumpad(int x, int y, int width, int height, bool movable,
                 // Numpad grid layout
                 (new Column)->SetScaleToFit(true)->AddChildren({
                     (new Rectangle)->SetFillColor(Color::black)->SetOutlineColor(highlightColor.get())->AddChildren({
-                        (new Text)->SetText("418")->SetAlignments(HorizontalAlign::center, VerticalAlign::middle)->SetOverflow(Overflow::wrapScale),
+                        (new Text)->SetText("418")->SetAlignments(HorizontalAlign::center, VerticalAlign::middle)->SetOverflow(Overflow::wrapScale)->SetPreTick(UpdateTypingTextNode),
                     }),
                     (new Row)->SetScaleToFit(true)->AddChildren({
-                        CreateKey('7'),
-                        CreateKey('8'),
-                        CreateKey('9'),
+                        CreateDigitKey(7),
+                        CreateDigitKey(8),
+                        CreateDigitKey(9),
                     }),
                     (new Row)->SetScaleToFit(true)->AddChildren({
-                        CreateKey('4'),
-                        CreateKey('5'),
-                        CreateKey('6'),
+                        CreateDigitKey(4),
+                        CreateDigitKey(5),
+                        CreateDigitKey(6),
                     }),
                     (new Row)->SetScaleToFit(true)->AddChildren({
-                        CreateKey('1'),
-                        CreateKey('2'),
-                        CreateKey('3'),
+                        CreateDigitKey(1),
+                        CreateDigitKey(2),
+                        CreateDigitKey(3),
                     }),
                     (new Row)->SetScaleToFit(true)->AddChildren({
                         (new Rectangle)->SetShallowID("intMessage")->SetDisabled(true)->SetFillColor(buttonNFocusedColor.get())->SetOutlineColor(highlightColor.get())->AddChildren({
                             (new Text)->SetText("Int")->SetFontSize(20)->SetAlignments(HorizontalAlign::center, VerticalAlign::middle)->SetOverflow(Overflow::wrapScale),
                         }),
-                        CreateSpecialKey(".")->SetShallowID("decimalKey")->SetDisabled(false),
-                        CreateKey('0'),
-                        CreateSpecialKey("<"),
+                        CreateSpecialKey(".")->SetShallowID("decimalKey")->SetDisabled(false)->SetRelease(StartTypingDecimal),
+                        CreateDigitKey(0),
+                        CreateSpecialKey("<")->SetRelease(RemoveDigitFromEnd),
                     }),
                 }),
             }),
